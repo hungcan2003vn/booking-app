743d2152f9be95cb3b32f19c9ca5b50a82c48119
hungcan2003vn <hunglubo113@gmail.com>
2024-08-02 01:21:15 +0700

    Delete src directory

--- src/redux/auth/authActions.ts
+++
@@ -1,5 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const loginRequest = createAction('LOGIN_REQUEST');
-export const loginSuccess = createAction<any>('LOGIN_SUCCESS');
-export const loginFailure = createAction<string>('LOGIN_FAILURE');
